4a11f874d658abfa300721542973cefb7d424b86
PogSmok <lukaszprus.job@gmail.com>
2026-03-04 17:35:35 +0100

    lab10/part5

---
+++ Digital_Logic/LAB10/part5/Simulator/testbench.tcl
@@ -0,0 +1,35 @@
+# stop any simulation that is currently running
+quit -sim
+
+# if simulating with a MIF file, copy it to the working folder.
+set source_mif "inst_mem.mif"
+# set source_mif "sitbooboosit.mif"
+if {[file exists ../$source_mif]} {
+	file delete inst_mem.mif
+	file copy ../$source_mif inst_mem.mif
+}
+# in case Quartus generated an "empty black box" file for the memory, delete it
+if {[file exists ../inst_mem_bb.v]} {
+	file delete ../inst_mem_bb.v
+}
+
+if {[file exists work]} {
+    file delete -force work
+}
+
+# create the default "work" library
+vlib work;
+
+# compile the Verilog source code in the parent folder
+vlog -nolock ../*.v
+# compile the Verilog source code in the src folder
+vlog -nolock ../src/*.v
+# compile the Verilog code of the testbench
+vlog -nolock *.v
+# start the Simulator
+vsim work.testbench -Lf 220model -Lf altera_mf_ver -Lf verilog
+# show waveforms specified in wave.do
+do wave.do
+
+# advance the simulation the desired amount of time
+run 2500 ns
